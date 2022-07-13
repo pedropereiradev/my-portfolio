823b20000f54519904ef965ea46757da7c1991a7
Pedro Pereira <pedro.2108@hotmail.com>
2022-07-13 00:29:40 -0300

    feat: Resposivity and anchor implemented

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,122 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import MenuIcon from '@mui/icons-material/Menu';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+
+// TODO: Linkar sections names com cada section
 
 function NavBar() {
+  const [anchor, setAnchor] = useState(false);
+
+  const sections = ['Skills', 'Projects', 'Contact me'];
+
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event
+      && event.type === 'keydown'
+      && (event.key === 'Tab' || event.key === 'Shift')
+    ) {
+      return;
+    }
+
+    setAnchor(open);
+  };
   return (
-    <header>
-      <nav>
-        <a href="#skills">Skills</a>
-        <a href="#projects">Projects</a>
-        <a href="#contact-me">Contact me</a>
-      </nav>
-    </header>
+    <AppBar position="static">
+      <Container>
+        <Toolbar disableGutters>
+          {/* Menu small screen < 600px */}
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', sm: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.1rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Pedro Pereira
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', sm: 'none' } }}>
+            <IconButton
+              color="inherit"
+              onClick={toggleDrawer(true)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <SwipeableDrawer
+              anchor="left"
+              open={anchor}
+              onClose={toggleDrawer(false)}
+              onOpen={toggleDrawer(true)}
+            >
+              <Box
+                sx={{ width: 250 }}
+                role="presentation"
+                onClick={toggleDrawer(false)}
+                onKeyDown={toggleDrawer(false)}
+              >
+                <List>
+                  {sections.map((text) => (
+                    <ListItem key={text} disablePadding>
+                      <ListItemButton>
+                        <ListItemText primary={text} />
+                      </ListItemButton>
+                    </ListItem>
+                  ))}
+                </List>
+              </Box>
+            </SwipeableDrawer>
+          </Box>
+          {/* Menu bigger then 600px */}
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 3,
+              display: { xs: 'flex', sm: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.1rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Pedro Pereira
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', sm: 'flex' }, justifyContent: 'flex-end' }}>
+            {sections.map((page) => (
+              <Button
+                key={page}
+                onClick={() => console.log('click')}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
 
