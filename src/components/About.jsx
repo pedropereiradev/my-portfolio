a03f235f158fbea02ce15f2783b226bad22ecf27
Pedro Pereira <pedro.2108@hotmail.com>
2022-07-13 00:30:09 -0300

    feat: image removed from About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
-import portfolioImage from '../assets/portfolioImage.png';
 
 function About() {
   return (
     <section>
-      <section>
-        <h1>Pedro Pereira</h1>
-        <p>
-          Hi there! I am a frontend developer and backend development student.
-          Here is my portfolio page where you can learn about my journey
-          as a full stack web developer.
-        </p>
-      </section>
-      <section>
-        <img src={portfolioImage} alt="Me" />
-      </section>
+      <h2> Hi there!</h2>
+      <h1>Pedro Pereira</h1>
+      <p>
+        I am a frontend developer and backend development student.
+        Here is my portfolio page where you can learn about my journey
+        as a full stack web developer.
+      </p>
     </section>
   );
 }
