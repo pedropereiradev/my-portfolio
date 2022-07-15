2f82cf6afd71ff23e77d57e76a71c99fb605a251
Pedro Pereira <pedro.2108@hotmail.com>
2022-07-15 00:08:30 -0300

    feat: Skills section implemented

---
+++ src/assets/techIcons.js
@@ -0,0 +1,69 @@
+export const techIcons = [
+  {
+    img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg',
+    title: 'HTML',
+  },
+  {
+    img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg',
+    title: 'CSS',
+  },
+  {
+    img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg',
+    title: 'Javascript',
+  },
+  {
+    img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+    title: 'React',
+  },
+  {
+    img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg',
+    title: 'Redux',
+  },
+  {
+    img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg',
+    title: 'Jest',
+  },
+  {
+    img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/argocd/argocd-original.svg',
+    title: 'RTL',
+  },
+  {
+    img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg',
+    title: 'Bootstrap',
+  },
+  {
+    img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-plain.svg',
+    title: 'Material UI',
+  },
+  {
+    img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-plain.svg',
+    title: 'Docker',
+  },
+  {
+    img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-plain.svg',
+    title: 'SQL',
+  },
+];
+
+export const learningTechIcons = [
+  {
+    img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain.svg',
+    title: 'Node.js',
+  },
+  {
+    img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain.svg',
+    title: 'MongoDB',
+  },
+  {
+    img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg',
+    title: 'Express',
+  },
+  {
+    img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mocha/mocha-plain.svg',
+    title: 'Mocha',
+  },
+  {
+    img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-plain.svg',
+    title: 'Python',
+  },
+];
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,23 +1,90 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+
+import Container from '@mui/material/Container';
+import { techIcons, learningTechIcons } from '../assets/techIcons';
 
 function Skills() {
   return (
-    <section>
-      <h2>Skills</h2>
-      <section>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>React</li>
-          <li>Redux</li>
-          <li>Jest</li>
-          <li>RTL</li>
-          <li>Bootstrap e React-Bootstrap</li>
-          <li>Docker</li>
-        </ul>
-      </section>
-    </section>
+    <Stack
+      direction={{ xs: 'column', sm: 'row' }}
+    >
+      <Container>
+        <Typography
+          component="h2"
+          variant="h2"
+          align="center"
+          sx={{
+            py: 3,
+            color: 'info.main',
+          }}
+        >
+          Skills
+        </Typography>
+        <Grid
+          container
+          rowSpacing={4}
+          columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+        >
+          {techIcons.map((icon) => (
+            <Grid item xs={4} align="center" key={icon.title}>
+              <Box
+                component="img"
+                src={icon.img}
+                alt={icon.title}
+                sx={{
+                  width: 52,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                }}
+              />
+              <Typography variant="caption" sx={{ color: 'text.disabled' }}>
+                {icon.title}
+              </Typography>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+      <Divider orientation="vertical" variant="middle" flexItem />
+      <Container>
+        <Typography
+          component="h2"
+          variant="h2"
+          align="center"
+          sx={{
+            py: 3,
+            color: 'info.main',
+          }}
+        >
+          Learning
+        </Typography>
+        <Grid container rowSpacing={4} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          {learningTechIcons.map((icon) => (
+            <Grid item xs={4} align="center" key={icon.title}>
+              <Box
+                component="img"
+                src={icon.img}
+                alt={icon.title}
+                sx={{
+                  width: 52,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                }}
+              />
+              <Typography variant="caption" sx={{ color: 'text.disabled' }}>
+                {icon.title}
+              </Typography>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </Stack>
   );
 }
 
