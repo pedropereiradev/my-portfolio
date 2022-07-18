86c905e13c7199925004292b04c8b1ab081f48ee
Pedro Pereira <pedro.2108@hotmail.com>
2022-07-17 23:32:57 -0300

    feat: add projects base

---
+++ src/services/projects.js
@@ -0,0 +1,63 @@
+const projects = [
+  {
+    title: 'My Personal Chef',
+    description:
+      'My personal Chef is a recipe app where you can find all kind of food and drinks, with step-by-step instructions. '
+      + 'It is also possible to apply search filters like, recipe name, category and nationality. The app just have mobile version, 360x640',
+    repositoryLink: 'https://github.com/pedropereiradev/my-personal-chef',
+    productionLink: 'https://mypersonalchef.vercel.app',
+    thumb: 'https://i.imgur.com/eml3Iw0.png',
+    pictures: {
+      img0: 'https://i.imgur.com/b7pOrIC.png',
+      img1: 'https://i.imgur.com/i8MSPzt.png',
+      img2: 'https://i.imgur.com/OmV0KeM.png',
+      img3: 'https://i.imgur.com/MXdhOJo.png',
+    },
+    technologies: [
+      'react',
+      'hooks',
+      'context API',
+      'reacto-router-dom',
+      'bootstrap',
+    ],
+  },
+  {
+    title: 'Trivia Game',
+    description:
+      'This app is a game where you have 30 seconds to answer 5 random questions. It is also possible to choose the category of the questions.',
+    repositoryLink: 'https://github.com/pedropereiradev/trivia-game',
+    productionLink: 'https://pedropereiradev-trivia.vercel.app',
+    thumb: 'https://i.imgur.com/ysdZugE.png',
+    pictures: {
+      img0: 'https://i.imgur.com/x5mYX75h.png',
+      img1: 'https://i.imgur.com/Kvma2kJh.png',
+      img2: 'https://i.imgur.com/zJvD8xoh.png',
+      img3: 'https://i.imgur.com/LSmr60Ih.png',
+    },
+    technologies: [
+      'react',
+      'redux',
+      'react-router-dom',
+      'jest',
+      'RTL',
+      'bootstrap',
+    ],
+  },
+  {
+    title: 'TrybeWallet',
+    description:
+      'This app is a digital wallet with an integrated currency converter, being able to add, edit, read and delete expenses. ',
+    repositoryLink: 'https://github.com/pedropereiradev/trybewallet',
+    productionLink: 'https://pedropereiradev-trybewallet.vercel.app',
+    thumb: 'https://i.imgur.com/SFrOQs4h.png',
+    pictures: {
+      img1: 'https://i.imgur.com/SFrOQs4h.png',
+      img2: 'https://i.imgur.com/ZYXGbUCh.png',
+      img3: '',
+      img4: '',
+    },
+    technologies: ['react', 'redux', 'react-router-dom', 'bootstrap'],
+  },
+];
+
+export default projects;
