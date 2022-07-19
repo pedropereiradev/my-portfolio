2a787a1287be12438e8d094c3fe2077fc031cecb
Pedro Pereira <pedro.2108@hotmail.com>
2022-07-18 21:23:58 -0300

    feat: implemented system preference theme and save theme preference in localStorage

--- src/context/Provider.jsx
+++ src/context/Provider.jsx
@@ -1,10 +1,24 @@
-import React, { useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
-import { createTheme } from '@mui/material';
+import { createTheme, useMediaQuery } from '@mui/material';
 import Context from './Context';
 
 function Provider({ children }) {
   const [mode, setMode] = useState('light');
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  useEffect(() => {
+    const userTheme = localStorage.getItem('theme');
+    if (!userTheme) {
+      localStorage.setItem('theme', mode);
+    } else {
+      setMode(userTheme);
+    }
+
+    if (prefersDarkMode) setMode('dark');
+  }, []);
+
+  useEffect(() => { localStorage.setItem('theme', mode); }, [mode]);
 
   const colorMode = useMemo(() => ({
     toggleColorMode: () => {
@@ -22,10 +36,6 @@ function Provider({ children }) {
     () => ({ colorMode, theme, mode }),
     [colorMode, theme, mode],
   );
-
-  /* const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-
-  console.log(prefersDarkMode); */
 
   return <Context.Provider value={contextProviderValue}>{ children }</Context.Provider>;
 }
